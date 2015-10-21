36e72219dd2c2c8475d451d61a8199e83f118ba9
krishnagadde23 <krishnagadde23@gmail.com>
2015-10-21 11:03:05 -0400

    Lab 08: Keep track of activities

--- app/src/main/java/com/example/kgadde/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/kgadde/activitylifecycle/MainActivity.java
@@ -8,27 +8,69 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int createCount, startCount, resumeCount, pauseCount, stopCount, restartCount, destroyCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createCount++;
+        TextView t = (TextView) findViewById(R.id.t1);
+        t.setText("onCreate(): " + createCount);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
+    protected void onStart()
+    {
+        super.onStart();
+        startCount++;
+        TextView t = (TextView) findViewById(R.id.t2);
+        t.setText("onStart(): " + startCount);
+    }
+    protected void onResume()
+    {
+        super.onResume();
+        resumeCount++;
+        TextView t = (TextView) findViewById(R.id.t3);
+        t.setText("onResume(): " + resumeCount);
+    }
+    protected void onPause()
+    {
+        super.onPause();
+        pauseCount++;
+        TextView t = (TextView) findViewById(R.id.t4);
+        t.setText("onPause(): " + pauseCount);
+    }
+    protected void onStop()
+    {
+        super.onStop();
+        stopCount++;
+        TextView t = (TextView) findViewById(R.id.t5);
+        t.setText("onCreate() : " + stopCount);
+    }
+    protected void onRestart()
+    {
+        super.onRestart();
+        restartCount++;
+        TextView t = (TextView) findViewById(R.id.t6);
+        t.setText("onRestart(): " + restartCount);
+    }
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        destroyCount++;
+        TextView t = (TextView) findViewById(R.id.t7);
+        t.setText("onDestroy(): " + destroyCount);
+    }
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
